733c0bcb7016d59697247fc3ed9b00783fb66593
Nguyễn Đặng Minh Duy <mduy88201010@gmail.com>
2025-06-27 13:10:07 +0700

    Initial React Project setup

---
+++ e-commerce/gomallclient/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+            <Navbar />
+    </div>
+  );
+}
+
+export default App;
