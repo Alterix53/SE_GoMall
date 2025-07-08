195b7150c625c724ed597051093777e85a6ae85b
Nguyễn Đặng Minh Duy <mduy88201010@gmail.com>
2025-07-08 17:32:26 +0700

    File name changes

--- e-commerce/gomallclient/src/Component/Footer/Footer.jsx
+++ e-commerce/gomallclient/src/Component/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Footer.css'; // Import the CSS file for styling
+import './footer.css'; // Import the CSS file for styling
 
 export default function Footer() {
   return (
