6c450fa64a179d0f2bd25f9aee7bf16d67726abe
unknown <phuoctqhlk@gmail.com>
2025-08-16 22:53:51 +0700

    Thêm api cho seller ở Server.js

--- e-commerce/server/Server.js
+++ e-commerce/server/Server.js
@@ -14,6 +14,7 @@ import momoPaymentRoutes from "./routes/momoPaymentRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
+import sellerRoutes from "./routes/sellerRoutes.js";
 import Product from './models/Product.js';
 import Category from './models/Category.js';
 import productService from './services/productService.js';
@@ -111,6 +112,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/momo", momoPaymentRoutes);  // Thêm MoMo payment routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/notifications", notificationRoutes);
+app.use("/api/seller", sellerRoutes);
 
 // app.use("/api/auth", userRoutes); // Mount auth routes trước
 
